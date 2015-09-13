5d43ad5bb0b51ab4d9a6aa2431e01bc0f9a8981a
Fabien Marteau <fabien.marteau@armadeus.com>
2015-09-13 18:48:16 +0200

    BrushlessModule: input outputs

--- hdl/brushless.scala
+++ hdl/brushless.scala
@@ -1,23 +1,30 @@
 import Chisel._
 
-class BrushlessModule(c: Clock = null) extends Module(c) {
+class Phase extends Bundle {
+    val p = Bool(OUTPUT)
+    val n = Bool(OUTPUT)
+}
+
+class BrushlessModule(c: Clock = null, freq: Int = 1250000) extends Module(c) {
   val io = new Bundle {
         /* parameters */
         val enable = Bool(INPUT)
+        val speed = SInt(INPUT, width=16) 
         /* output */
-        val pwmout = Bool(OUTPUT)
+        val phases = Vec.fill(3){new Phase()}
   }
-
-  io.pwmout := io.enable
+  io.phases(0).p := Bool(true)
 }
 
 class BrushlessTests(c: BrushlessModule) extends Tester(c) {
-  step(1)
+    poke(c.io.enable, 1)
+    step(10)
 }
 
 object brushless {
   def main(args: Array[String]): Unit = {
-    chiselMainTest(Array[String]("--backend", "c", "--compile", "--test", "--genHarness"),
+      Driver.implicitClock.period=8000
+      chiselMainTest(Array[String]("--backend", "c", "--compile", "--test", "--genHarness", "--vcd"),
        () => Module(new BrushlessModule())){c => new BrushlessTests(c)}
   }
 }
