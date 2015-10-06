4ef0842c3c1c8eabdc09de58135ac7e945e4437c
Fabien Marteau <fabien.marteau@armadeus.com>
2015-10-06 10:35:13 +0200

    adding states machine diagram

--- hdl/brushless.scala
+++ hdl/brushless.scala
@@ -13,7 +13,14 @@ class BrushlessModule(c: Clock = null, freq: Int = 1250000) extends Module(c) {
         /* output */
         val phases = Vec.fill(3){new Phase()}
   }
+
+
+  val count = Reg(init=UInt(0, width=6))
+
+  count := count + UInt(1)
+
   io.phases(0).p := Bool(true)
+
 }
 
 class BrushlessTests(c: BrushlessModule) extends Tester(c) {
@@ -23,7 +30,6 @@ class BrushlessTests(c: BrushlessModule) extends Tester(c) {
 
 object brushless {
   def main(args: Array[String]): Unit = {
-      Driver.implicitClock.period=8000
       chiselMainTest(Array[String]("--backend", "c", "--compile", "--test", "--genHarness", "--vcd"),
        () => Module(new BrushlessModule())){c => new BrushlessTests(c)}
   }
